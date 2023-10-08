eac123a37f61c910f1531d3bb865ed54d8e069dc
atikurrahman33 <foysalatik33@gmail.com>
2023-10-08 10:15:41 +0600

    creat services card

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,10 +1,28 @@
+import './Service.css'
 
-
-const Service = () => {
+const Service = ({items}) => {
+    const{id, name, price,image,description }=items;
     return (
-        <div>
-            <h1>his page</h1>
+        <div className='pt-11'>
+           
+            <div className='card'>
+        <div className='card-img'>
+                    <img className='photo' src={image} alt="" />
+                     </div>
+                     <div>
+                        <h2>{name}</h2>
+                        <p>{description}</p>
+                     </div>
+                     <div className='info'>
+                        <p> $ Price: {price} </p>
+                        </div>
+                        <button   className='card-btn'>Select</button>
+
+
+            </div>
         </div>
+    
+   
     );
 };
 
